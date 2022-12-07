5a0553847e62aa6dc5510c393a378eab06136347
DNUVision <davidfischsohn@gmail.com>
2022-12-07 23:19:19 +0200

    recommit after commit failed; updated files & style.css created

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Rock-Paper-Scissors-Project
 Rock Paper Scissors Project for The Odin Project Foundations Path
+Vanilla JS(DOM Practice)
--- scripts.js
+++ scripts.js
@@ -56,6 +56,7 @@ function playRound(player, computer){
     appender.appendChild(newElem);
     totalScore.textContent = "Computer: " + computerCounter + ", Player: " + playerCounter + ".";
     appender.appendChild(totalScore);
+    
 }
 
 //inside the function game(), I call playRound() for 5 rounds in the console. 
