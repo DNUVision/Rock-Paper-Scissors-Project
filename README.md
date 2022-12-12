1df69b94f3e0285a7ae6234127307bddf99b866e
DNUVision <davidfischsohn@gmail.com>
2022-12-12 14:43:15 +0200

    final touches before merging branches

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Rock-Paper-Scissors-Project
 Rock Paper Scissors Project for The Odin Project Foundations Path
-Vanilla JS(DOM Practice)
+Vanilla JS(DOM Practice
+
+This is a Rock, Paper, Scissors game
+The computer chooses a value randomly
+While the player click on a button that holds a value same as the button.
+the two values are being compared
+and a result is generated in a new div
+the first who reaches 5 points wins
+and then the game resets(by clicking on one of the buttons)
+and a new round is set.
--- scripts.js
+++ scripts.js
@@ -1,23 +1,8 @@
-/*
-This is a Rock, Paper, Scissors game
-The computer chooses a value randomly
-The player is prompted to choose a value 
-if the player chose Rock and the computer chose Paper, then computer wins
-if the player chose Rock and the computer chose Scissors, then player wins
-if the player and the computer both chose Rock, then it is a tie.
-same will be for player choosing Scissors or Paper
-each time there is a result
-the result is kept to that turn's winner
-the game will be played for 5 turns
-*/
-
-
 /*The function decleration presented below is the computer's choice.
   random and floor methods are being used to generate a value between 0 and 2
   afterwards there is a switch statmenet that assigns each number to a value */    
 function getComputerChoice(){
     const comSelection = Math.floor(Math.random()*3);
-    //console.log(comSelection);
     let result;
     switch(comSelection){
         case 0:
@@ -34,14 +19,15 @@ function getComputerChoice(){
 }
 
 /* two variables initialized and will be used to count the score
-   I used function playRound() to pass the prior two functions as callbacks
-   the callbacks return the values stored in them and they are compared
-   in the if statmenet, and each one is getting a score accordingly */
+   I declare function playRound() and give (player, computer) parameters
+   each round's result and total rounds' result wil be presented on a created div */
 
 let playerCounter = 0;
 let computerCounter = 0;   
 const newElem = document.createElement("div");
-const totalScore = document.createElement("div"); 
+const totalScore = document.createElement("div");
+newElem.classList.add("score");
+totalScore.classList.add("score"); 
 function playRound(player, computer){
     if((player == "ROCK" && computer == "PAPER") || (player == "PAPER" && computer == "SCISSORS") || (player == "SCISSORS" && computer == "ROCK")){
         newElem.textContent = "Player lost, Computer wins!";
@@ -56,17 +42,12 @@ function playRound(player, computer){
     appender.appendChild(newElem);
     totalScore.textContent = "Computer: " + computerCounter + ", Player: " + playerCounter + ".";
     appender.appendChild(totalScore);
-    
+
 }
 
-//inside the function game(), I call playRound() for 5 rounds in the console. 
+/*inside the function game(), I call playRound(), the game will go on until one has reached 5 points.
+and then using if statements the score will reset itself*/
 function game(){
-    //for(let i = 0; i < 5; i++){
-        //console.log("Round #" + (i+1));
-        //console.log("Current Player's score is: " + playerCounter);
-        //console.log("Current Computer's score is: " + computerCounter); 
-        //playRound(getPlayerChoice(), getComputerChoice());
-    //}
     let select;
     const i = document.querySelectorAll("button");
     const looper = i.forEach((val)=>{
@@ -86,62 +67,6 @@ function game(){
             }
         });
     });
-
-
-
-    /*console.log("After five rounds the overall score is \nPlayer score: " + playerCounter + " vs Computer score: " + computerCounter);
-    if(playerCounter > computerCounter){
-        console.log("Player won the five round tournament!");
-    }else if(computerCounter > playerCounter){
-        console.log("COmputer won the five round tournament!");
-    }else{
-        console.log("No one won, the five round tournament ended in a DRAW!");
-    }
-    */
 }
-
-
-
-    
+   
 game();
-
-
-/*const btnFocus = document.querySelectorAll("button");
-btnFocus.forEach((btnObj) => {
-    btnObj.addEventListener("click", getPlayerChoice);
-
-    function myChoice(){
-        const newElem = document.createElement("div");
-        newElem.textContent = btnObj.textContent;
-        document.body.appendChild(newElem);
-    }
-}); */
-//The function decleration presented below is the player's choice using promot, converting to uppercase and storing the value if the user entered it correctly. 
-//function getPlayerChoice(){
-//    let selection;
-//    let userResult;
-    
-
-
-
-   /* const btnFocus = document.querySelectorAll("button");
-    btnFocus.forEach((btnObj) => {
-        btnObj.addEventListener("click", (e) =>{
-            selection = e.target.value;
-            //let selection = prompt("Please choose either Rock, Scissors or Paper: ").toUpperCase();
-            if(selection == "ROCK" || selection == "SCISSORS" || selection == "PAPER"){
-            console.log("This is the user's selection: " + selection);
-            userResult = selection;
-            return userResult;
-            }});
-    });*/
-    //let selection = prompt("Please choose either Rock, Scissors or Paper: ").toUpperCase();
-   //if(selection == "ROCK" || selection == "SCISSORS" || selection == "PAPER"){
-   //     console.log("This is the user's selection: " + selection);
-   //     userResult = selection;
-   //     return userResult;
-   // }/*else{
-   //     alert("You chose wrong. Try again please.");
-   //     return getPlayerChoice();
-   //     }  */
-   // }
